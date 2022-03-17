c3bb81c6451e3bc55b83d72ccaf64a8aa28ea140
BuildTools <unconfigured@null.spigotmc.org>
2022-03-17 17:07:24 +0900

    3월17일 수정

--- chatClient/chatClient.cpp
+++ chatClient/chatClient.cpp
@@ -1,11 +1,40 @@
-﻿// chatClient.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿//목요드라마 석 & 훈
 
 #include <iostream>
+#include <thread>
+#include <mutex>
+#include <string>
+#include <chrono>
 
-int main()
+using namespace std;
+
+
+string taesu = "솔아..";
+mutex taesu_maum;
+
+void Propose(string my_name, string his_name)
 {
-    std::cout << "0317수정 ChatClinet!\n";
+	cout << "\n\n";
+	cout << my_name << ": " << his_name << ".. 당신을 진심으로 사랑합니다.." << endl;
+	cout << my_name << ": " << his_name << "..날 가져줘.." << endl;
+	this_thread::sleep_for(std::chrono::milliseconds(1000));
 }
 
+void GetLove(string name)
+{
+	taesu_maum.lock();
+	taesu = name;
+	Propose("수", taesu);
+	this_thread::sleep_for(std::chrono::milliseconds(5000));//5년
+	cout << "수 :" << name << " 우리 헤어져";
+	taesu_maum.unlock();
+}
 
+void main() //Thread0 PD : 이은석 (main)
+{
+	thread na_seok_hoon(GetLove, "석훈아.."); //Thread2
+	thread oh_jeong_seok(GetLove, "정석아.."); //Thread1 
+
+	na_seok_hoon.join();
+	oh_jeong_seok.join();
+}
