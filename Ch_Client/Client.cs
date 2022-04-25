795aa8d9dbde346e6bbf8b5890bc2b0e6653f0a7
LMSsub <lim0492@naver.com>
2022-04-26 03:14:58 +0900

    그냥 죽여주세요 제발 걍 이대로 낼래요

--- Ch_Client/Client.cs
+++ Ch_Client/Client.cs
@@ -6,48 +6,64 @@ public class MyTcpClient
 
     public static void Main()
     {
-        Console.WriteLine("나는클라이언트");
+        bool state = true;
+        Console.WriteLine("나는클라이언트 IP를 입력해주세요 127.0.0.1");
         try
         {
+            while (state = true)
+            {
+                /*string server = "127.0.0.1";*/
+                string server = Console.ReadLine();
+            
+                if (server == "127.0.0.1")
+                {
+                    state = false;
+                    Console.WriteLine("127.0.0.1에 접속시도중... ");
+                    Console.WriteLine("'주'님께 연결되었습니다.");
+                    string message = Console.ReadLine();
+                   
+                    Int32 port = 13000;
+                    TcpClient client = new TcpClient(server, port);
+                    // TcpClient를 만듭니다.
+                    // 이 클라이언트가 작동하려면 TcpServer가 필요합니다.
+                    // 서버, 포트에서 지정한 것과 동일한 주소에 연결
+                    // combination.%
+                    //전달된 메시지를 ASCII로 변환하고 바이트 배열로 저장합니다.
+                    Byte[] data = System.Text.Encoding.Default.GetBytes(message);
 
-            string server = "127.0.0.1";
-            string message = Console.ReadLine();
+                    // 읽고 쓰기 위한 클라이언트 스트림을 가져옵니다.
+                    // Stream stream = client.GetStream();
+                    NetworkStream stream = client.GetStream();
+                    // 연결된 TcpServer로 메시지를 보냅니다.
+                    stream.Write(data, 0, data.Length);
+                    
+                    // TcpServer.response를 수신합니다.
+                    // 응답 바이트를 저장할 버퍼입니다.
+                    data = new Byte[256];
+                    // 응답 UTF8 표현을 저장할 문자열입니다.
 
-            // TcpClient를 만듭니다.
-            // 이 클라이언트가 작동하려면 TcpServer가 필요합니다.
-            // 서버, 포트에서 지정한 것과 동일한 주소에 연결
-            // combination.%
-            Int32 port = 13000;
-            TcpClient client = new TcpClient(server, port);
+                    String responseData = String.Empty;
+                    // TcpServer 응답 바이트의 첫 번째 배치를 읽습니다.
+                    Int32 bytes = stream.Read(data, 0, data.Length);
+                    responseData = System.Text.Encoding.UTF8.GetString(data, 0, bytes);
+                    Console.WriteLine("[수]클라: {0}", responseData);
+                    Console.WriteLine("[주]서버: {0}",responseData);
+                    
+                }
+                else
+                {
+                    Console.WriteLine("서버가 연결되지 않았습니다 주소를 올바르게 입력했는지 확인해 주세요");
+                    state = true;
 
-            //전달된 메시지를 ASCII로 변환하고 바이트 배열로 저장합니다.
-            Byte[] data = System.Text.Encoding.Default.GetBytes(message);
+                }
+            }
 
-            // 읽고 쓰기 위한 클라이언트 스트림을 가져옵니다.
-            // Stream stream = client.GetStream();
-            NetworkStream stream = client.GetStream();
-            // 연결된 TcpServer로 메시지를 보냅니다.
-            stream.Write(data, 0, data.Length);
-
-            // TcpServer.response를 수신합니다.
-            // 응답 바이트를 저장할 버퍼입니다.
-            data = new Byte[256];
-            // 응답 UTF8 표현을 저장할 문자열입니다.
-            String responseData = String.Empty;
-            // TcpServer 응답 바이트의 첫 번째 배치를 읽습니다.
-            Int32 bytes = stream.Read(data, 0, data.Length);
-            responseData = System.Text.Encoding.UTF8.GetString(data, 0, bytes);
-            Console.WriteLine("[수]클라: {0}", responseData);
-            Console.WriteLine("[주]서버: {0}", message);
-
-            // 모든 것을 닫습니다.
-            stream.Close();
-            /*client.Close();*/
+            
         }
-        /*catch (ArgumentNullException e)
+        catch (ArgumentNullException e)
         {
             Console.WriteLine("ArgumentNullException: {0}", e);
-        }*/
+        }
         catch (SocketException e)
         {
             Console.WriteLine("SocketException: {0}", e);
--- Ch_Seerver/Sever.cs
+++ Ch_Seerver/Sever.cs
@@ -7,74 +7,76 @@ class MyTcpListener
     public static void Main()
     {
 
-        Console.WriteLine("나는 서버");
+        Console.WriteLine("Waiting Connection...");
         TcpListener server = null;
-        try
+        while (true)
         {
-            // Set the TcpListener on port 13000. 포트 13000에서 TcpListener를 설정합니다.
-            Int32 port = 13000;
-            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
-
-            // TcpListener server = new TcpListener(port); TcpListener 서버 = 새로운 TcpListener(포트);
-            server = new TcpListener(localAddr, port);
-
-            // Start listening for client requests. 클라이언트 요청 수신을 시작합니다.
-            server.Start();
-
-            // Buffer for reading data
-            Byte[] bytes = new Byte[256];
-            String Cdata = null;
-            String Sdata = null;
-
-            // Enter the listening loop.
-            while (true)
+            try
             {
-                Console.Write("잠시만 기다려 주세요 연결 중입니다.... \n");
+                // Set the TcpListener on port 13000. 포트 13000에서 TcpListener를 설정합니다.
+                Int32 port = 13000;
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
 
-                // Perform a blocking call to accept requests.
-                // You could also use server.AcceptSocket() here.
-                TcpClient client = server.AcceptTcpClient();
-                Console.WriteLine("연결되었습니다.!");
+                // TcpListener server = new TcpListener(port); TcpListener 서버 = 새로운 TcpListener(포트);
+                server = new TcpListener(localAddr, port);
 
-                Cdata = null;
-                Sdata = null;
+                // Start listening for client requests. 클라이언트 요청 수신을 시작합니다.
+                server.Start();
 
+                // Buffer for reading data
+                Byte[] bytes = new Byte[256];
+                String Cdata = null;
 
-                // 읽고 쓰기 위한 스트림 객체 가져오기
-                NetworkStream stream = client.GetStream();
-
-                int i;
-
-                // 클라이언트가 보낸 모든 데이터를 수신하는 루프
-                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                // Enter the listening loop.
+                while (true)
                 {
-                    // Translate data bytes to a UTF8 string.
-                    Cdata = System.Text.Encoding.UTF8.GetString(bytes, 0, i);
-                    Console.WriteLine("[수]클라: {0}", Cdata);
+                    Console.Write("잠시만 기다려 주세요 연결 중입니다.... \n");
 
-                    // 클라이언트가 보낸 데이터를 처리합니다.
-                    Cdata = Cdata.ToUpper();
+                    // Perform a blocking call to accept requests.
+                    // You could also use server.AcceptSocket() here.
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("'수' 님이 127.0.0.1에서 접속하셨습니다");
 
-                    byte[] msg = System.Text.Encoding.UTF8.GetBytes(Cdata);
+                    Cdata = null;
+                    
 
-                    // 응답을 다시 보냅니다.
-                    stream.Write(msg, 0, msg.Length);
-                    Console.WriteLine("[주]서버: {0}", Sdata);
+                    // 읽고 쓰기 위한 스트림 객체 가져오기
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    // 클라이언트가 보낸 모든 데이터를 수신하는 루프
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Translate data bytes to a UTF8 string.
+                        Cdata = System.Text.Encoding.UTF8.GetString(bytes, 0, i);
+                        Console.WriteLine("[수]클라: {0}", Cdata);
+                        // 클라이언트가 보낸 데이터를 처리합니다.
+                        Cdata = Cdata.ToUpper();
+                        string Sdata = Console.ReadLine();
+
+                        byte[] msg = System.Text.Encoding.UTF8.GetBytes(Cdata);
+
+                        // 응답을 다시 보냅니다.
+                        stream.Write(msg, 0, msg.Length);
+                        Console.WriteLine("[주]서버: {0}", Sdata);
+                    }
+
+                    // 종료 및 연결 종료
+                    /*client.Close();*/
                 }
-
-                // 종료 및 연결 종료
-                /*client.Close();*/
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
             }
         }
-        catch (SocketException e)
+        
+       /* finally
         {
-            Console.WriteLine("SocketException: {0}", e);
-        }
-        /* finally
-         {
-             // Stop listening for new clients.
-             server.Stop();
-         }*/
+            // Stop listening for new clients.
+            server.Stop();
+        }*/
 
         /*Console.WriteLine("\nHit enter to continue...");
         Console.Read();*/
